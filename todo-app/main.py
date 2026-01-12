87f4315e025964bb2764a591ed537c88d0b3f001
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2026-01-12 20:35:13 +0200

    submission: 2.2

--- todo-app/main.py
+++ todo-app/main.py
@@ -1,14 +1,16 @@
 import os
-import requests
 import time
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="static")
 
 def refetch_image():
-    # Get image from Picsum and save it to static folder as hourly_image.png
+    """
+    Fetches a new image from Picsum and saves it to the static folder.
+    """
     img_url = 'https://picsum.photos/1200'
-    response = requests.get(img_url)
+    response = requests.get(img_url, timeout=10)
 
     if response.status_code == 200:
         img_path = os.path.join(app.static_folder, 'hourly_image.png')
@@ -17,17 +19,25 @@ def refetch_image():
 
 @app.get('/')
 def index():
-    hardcoded_todos = ["Learn Kubernetes", "Learn Flask", "Build awesome apps"]
-    return render_template('index.html', todos=hardcoded_todos)
+    """
+    Fetch todos from the backend and render them in the template.
+    """
+    response = requests.get('http://todo-backend-svc:8000/api/todos', timeout=5)
+    todos = response.json().get('todos', [])
+
+    return render_template('index.html', todos=todos)
 
 @app.get('/get-img')
 def get_img():
+    """
+    Serves an image that is refreshed every 10 minutes.
+    """
     # Get local file from static folder and check when it was last modified
     img_path = os.path.join(app.static_folder, 'hourly_image.png')
 
     if not os.path.exists(img_path):
         refetch_image()
-    
+
     # Check if the image is older than 10 minutes (600 seconds)
     last_modified_time = os.path.getmtime(img_path)
     current_time = time.time()
---
+++ todo-backend/main.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+
+todo_list = []
+
+@app.get('/api/todos')
+def get_todos():
+    """
+    Gets the list of todos from memory.
+    """
+
+    return {'todos': todo_list}
+
+@app.post('/api/todos')
+def add_todo():
+    """
+    Adds a new todo from a standard HTML form.
+    """
+    # Use request.form to get data from HTML <input name="name" />
+    todo_name = request.form.get('name')
+
+    if todo_name:
+        # Creating the object structure
+        new_todo = {
+            "name": todo_name,
+            "done": False
+        }
+        todo_list.append(new_todo)
+        
+        # Redirect back to the frontend home page
+        return redirect('/') 
+    
+    return {'error': 'No todo provided'}, 400
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    print(f"Server started in port {port}")
+    app.run(host='0.0.0.0', port=port)
