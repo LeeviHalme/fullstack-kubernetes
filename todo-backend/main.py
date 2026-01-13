11d2e03445a1e5694d9e5bf78e5d3b0ed28e88cc
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2026-01-13 21:01:25 +0200

    submission: 2.8

--- todo-backend/main.py
+++ todo-backend/main.py
@@ -1,17 +1,49 @@
 import os
+import psycopg2
 from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
-todo_list = []
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_db_connection():
+    """Establish a connection to the PostgreSQL database."""
+    return psycopg2.connect(DATABASE_URL)
+
+def init_db():
+    """Initialize the database and create the todo table if it doesn't exist."""
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("CREATE TABLE IF NOT EXISTS todos ( \
+                  id SERIAL PRIMARY KEY, \
+                  name TEXT NOT NULL, \
+                  done BOOLEAN NOT NULL DEFAULT FALSE \
+                );")
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+# Initialize DB at the top level for Gunicorn
+init_db()
 
 @app.get('/api/todos')
 def get_todos():
     """
-    Gets the list of todos from memory.
+    Gets the list of todos from db.
     """
 
-    return {'todos': todo_list}
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT id, name, done FROM todos;")
+    todos = [{"id": row[0], "name": row[1], "done": row[2]} for row in cur.fetchall()]
+
+    cur.close()
+    conn.close()
+
+    return { "todos": todos }, 200
 
 @app.post('/api/todos')
 def add_todo():
@@ -22,12 +54,14 @@ def add_todo():
     todo_name = request.form.get('name')
 
     if todo_name:
-        # Creating the object structure
-        new_todo = {
-            "name": todo_name,
-            "done": False
-        }
-        todo_list.append(new_todo)
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        cur.execute("INSERT INTO todos (name, done) VALUES (%s, %s);", (todo_name, False))
+
+        conn.commit()
+        cur.close()
+        conn.close()
 
         # Redirect back to the frontend home page
         return redirect('/')
